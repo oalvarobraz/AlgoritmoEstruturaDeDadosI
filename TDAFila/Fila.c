f9e97af6c97e3c1840740a52dc9db36edcf460f4
oalvarobraz <114008085+oalvarobraz@users.noreply.github.com>
2022-09-29 10:26:00 -0300

    Update Fila.c

--- TDAFila/Fila.c
+++ TDAFila/Fila.c
@@ -1,121 +1,138 @@
-
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 #include "fila.h"
 
-void FFVazia(TFila *Fila){
-    Fila->frente = (TCelula*) malloc(sizeof(TCelula));
+
+void FFVazia (TFila *Fila)
+{
+    Fila->frente = (TCelula *)malloc(sizeof(TCelula));
     Fila->tras = Fila->frente;
-    Fila->tras->prox = NULL;
+    Fila->frente->prox = NULL;
     Fila->tamanho = 0;
 }
 
-int Vazia(TFila Fila){
-    return (Fila.frente == Fila.tras);
+int Vazia(TFila Fila)
+{
+    return (Fila.tras == Fila.frente);
 }
 
-int GetTamanho(TFila Fila){
+int Tamanho(TFila Fila)
+{
     return Fila.tamanho;
 }
 
-void INQUEUE(TFila *Fila, TProduto x){
-    Fila->tras->prox = (TCelula*) malloc(sizeof(TCelula));
+void Enfileirar (TProduto x, TFila *Fila)
+{
+    Fila->tras->prox = (TCelula*)malloc(sizeof(TCelula));
     Fila->tras = Fila->tras->prox;
     Fila->tras->item = x;
     Fila->tras->prox = NULL;
     Fila->tamanho++;
 }
 
-void DEQUEUE(TFila *Fila, TProduto *x){
-    if(!Vazia(*Fila)){
+void Desenfileirar (TFila *Fila, TProduto *Item)
+{
+    if(!Vazia(*Fila))
+    {
         TCelula *Aux;
         Aux = Fila->frente->prox;
         Fila->frente->prox = Aux->prox;
-        *x = Aux->item;
+        *Item = Aux->item;
         free(Aux);
-        if(Fila->frente->prox = NULL)
-            Fila->tras = Fila->frente;
+        if (Fila->frente->prox == NULL)
+        {
+            Fila->tras = Fila->frente; // Tratamento de excessao
+        }
         Fila->tamanho--;
+    } else {
+        Item->codigo = -1;
     }
 }
 
 void LerProduto(TProduto *x)
 {
-
-    printf("\n|| Informe um codigo:");
+    printf("->Digite codigo do produto: ");
+    //__fpurge(stdin);
     fflush(stdin);
     scanf("%d", &x->codigo);
 
+    printf("->Digite o nome do produto: ");
+    //__fpurge(stdin);
+    fflush(stdin);
+    fgets(x->nome,100,stdin);
+
+    printf("->Digite a quantidade: ");
+    //__fpurge(stdin);
+    fflush(stdin);
+    scanf("%d", &x->quantidade);
+
+    printf("->Digite o preco: ");
+    //__fpurge(stdin);
+    fflush(stdin);
+    scanf("%d", &x->preco);
+    printf("\n");
 }
 
-//Função Auxiliar, visto que o utilzia o produto
 void ImprimirProduto(TProduto x)
 {
     printf("\n|| Codigo: %d\n", x.codigo);
+    printf("|| Nome: %s", x.nome);
+    printf("|| Quantidade: %d\n", x.quantidade);
+    printf("|| Preco: %d\n\n", x.preco);
 }
 
-void Imprimir(TFila *F1){
-    int n = GetTamanho(*F1);
+void Liberar (TFila *Fila)
+{
     TProduto x;
-    while(n!=0){
-        DEQUEUE(F1,&x);
-        ImprimirProduto(x);
-        INQUEUE(F1,x);
-        n--;
-    }
-}
-
-void Imprimir2(TFila *F1){
-    TProduto x;
-    TFila F2;
-    FFVazia(&F2);
-    while(!Vazia(*F1)){
-        DEQUEUE(F1,&x);
-        INQUEUE(&F2,x);
-    }
-    while(!Vazia(F2)){
-        DEQUEUE(&F2,&x);
-        ImprimirProduto(x);
-        INQUEUE(F1,x);
-    }
-    free(F2.frente);
-}
-
-TProduto Pesquisar(TFila *Fila, TProduto Item){
-    int n = GetTamanho(*Fila);
-    TProduto x, aux;
-    while (n != 0){
-        DEQUEUE(Fila,&x);
-        if(x.codigo == Item.codigo)
-            aux.codigo = Item.codigo;
-        INQUEUE(Fila,x);
-        n--;
-    }
-    return aux;
-}
-
-
-TProduto Pesquisar2(TFila *Fila, TProduto x){
-    TProduto y;
-    int n = GetTamanho(*Fila);
-    while (n!=0){
-        TCelula *Aux;
-        Aux = Fila->frente->prox;
-        DEQUEUE(Fila,&y);
-        if(Aux->item.codigo == x.codigo){
-            return Aux->item;
-        }
-        INQUEUE(Fila,y);
-        n--;
-    }
-}
-
-
-void Liberar(TFila *Fila){
-    TProduto item;
-    while(!Vazia(*Fila)){
-        DEQUEUE(Fila,&item);
+    while (!Vazia(*Fila))
+    {
+        Desenfileirar(Fila, &x);
     }
     free(Fila->frente);
+}
+
+void Imprimir1 (TFila *Fila1)
+{
+    TProduto x;
+    TFila Fila2;
+    FFVazia(&Fila2);
+    while (!Vazia(*Fila1))
+    {
+        Desenfileirar(Fila1, &x);
+        Enfileirar(x, &Fila2);
+    }
+    while (!Vazia(Fila2))
+    {
+        Desenfileirar(&Fila2, &x);
+        ImprimirProduto(x);
+        Enfileirar(x, Fila1);
+    }
+    Liberar(&Fila2);
+}
+
+void Imprimir2 (TFila *Fila)
+{
+    int n = Tamanho(*Fila);
+    TProduto x;
+    while (n != 0)
+    {
+        Desenfileirar (Fila, &x);
+        ImprimirProduto(x);
+        Enfileirar (x, Fila);
+        n--;
+    }
+}
+
+TProduto Pesquisar (TFila *Fila1, TProduto Item) {
+    TProduto x, Aux;
+    int n = Tamanho(*Fila1);
+    while (n != 0) {
+        Desenfileirar(Fila1, &x);
+        if(x.codigo == Item.codigo)
+            Aux.codigo = Item.codigo;
+        Enfileirar (x, Fila1);
+        n--;
+    }
+    return Aux;
 }
