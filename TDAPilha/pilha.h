1957415e57cb9d73cada69c8e14f5feeea2eb7d5
oalvarobraz <114008085+oalvarobraz@users.noreply.github.com>
2022-09-30 08:22:37 -0300

    Create pilha.h

---
+++ TDAPilha/pilha.h
@@ -0,0 +1,49 @@
+#ifndef PILHA_H_INCLUDED
+#define PILHA_H_INCLUDED
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct item
+{
+    int codigo;
+    char nome[100];
+    int quantidade;
+    float preco;
+} TProduto;
+
+typedef struct celula
+{
+    TProduto item;
+    struct celula* prox;
+} TCelula;
+
+typedef struct pilha
+{
+    TCelula* fundo;
+    TCelula* topo;
+    int tamanho;
+} TPilha;
+
+void FPVazia(TPilha *Pilha);
+
+int Vazia(TPilha Pilha);
+
+int GetTamanho(TPilha Pilha);
+
+void Empilhar(TProduto x, TPilha *Pilha);
+
+void Desempilhar(TPilha *Pilha, TProduto *item);
+
+void LerProduto(TProduto *x);
+
+void ImprimirProduto(TProduto x);
+
+void Liberar(TPilha *Pilha);
+
+void Imprimir(TPilha *Pilha);
+
+TProduto Pesquisar(TProduto Item, TPilha *Pilha);
+
+
+#endif // PILHA_H_INCLUDED
