a44f6597950f01cfe332bf3a5060fa2d311efeac
oalvarobraz <114008085+oalvarobraz@users.noreply.github.com>
2022-09-30 08:22:55 -0300

    Create interface.h

---
+++ TDAPilha/interface.h
@@ -0,0 +1,15 @@
+#ifndef INTERFACE_H_INCLUDED
+#define INTERFACE_H_INCLUDED
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "pilha.h"
+
+void MSG_MENU(); // MENSAGEM PARA EXIBIR O MENU PRINCIPAL
+
+void MENU(TPilha *pilha); // MÓDULO DE MENU
+
+
+#endif // INTERFACE_H_INCLUDED
